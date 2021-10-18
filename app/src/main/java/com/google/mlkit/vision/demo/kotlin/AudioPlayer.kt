497a03fe3b39914af816beac6970298bdf2b88b9
Sabit Ahmed <sabit.a.sirat@gmail.com>
2021-10-18 19:36:39 +0600

    implement voice incorrectly, add AudioPlayer class, include it in CameraXLivePreviewActivity.class

--- app/src/main/java/com/google/mlkit/vision/demo/kotlin/AudioPlayer.kt
+++ app/src/main/java/com/google/mlkit/vision/demo/kotlin/AudioPlayer.kt
@@ -2,16 +2,26 @@ package com.google.mlkit.vision.demo.kotlin
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.util.Log
 import androidx.annotation.RawRes
 
+
 class AudioPlayer(
-    private val context: Context
+    private val context: Context,
+    private var lastTimePlayed: Int = System.currentTimeMillis().toInt()
 ) {
     fun play(@RawRes filepath: Int) {
-        val player = MediaPlayer.create(context, filepath)
-        player.start()
-        player.setOnCompletionListener {
-            player.release()
+        val timestamp = System.currentTimeMillis().toInt()
+
+        if (timestamp - lastTimePlayed >= 5000) {
+            val player = MediaPlayer.create(context, filepath)
+            player.start()
+            player.setOnCompletionListener {
+                player.release()
+            }
+            Log.d("lastTimePlay", "${timestamp - lastTimePlayed}")
+            lastTimePlayed = timestamp
         }
     }
+
 }
